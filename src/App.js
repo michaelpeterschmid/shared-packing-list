417dfc985614cf560609932716ff2fdaa4c568d0
Michael Peter Schmid <michaelpeter.schmid17@gmail.com>
2025-11-20 16:02:53 +0100

    added base styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
+      <h1>Ttile h1</h1>
+      <h2>Ttile h2</h2>
+      <h3>Ttile h3</h3>
+      <h3>Ttile h3</h3>
+      <h4>Ttile h4</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia
+        perferendis necessitatibus repellat at, et maxime amet dicta quas
+        exercitationem porro esse repudiandae nesciunt similique veniam dolor
+        dolores quam qui possimus.
+      </p>
+      <button>subscibe</button>
+      <a href="https://michaelpeterschmid.github.io/portfolio/">
+        Link to portfolio
+      </a>
     </div>
   );
 }
