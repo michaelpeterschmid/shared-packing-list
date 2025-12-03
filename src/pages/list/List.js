40ea9603c95cdaec82769c96c122b277bb4470a2
Michael Peter Schmid <michaelpeter.schmid17@gmail.com>
2025-12-03 10:40:19 +0100

    useDocument inside List.js component

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+//custom hook to read a document in realtime
+import { useDocument } from "../../hooks/useDocument.js";
+
 const List = () => {
   const { id } = useParams();
+  const { document, error } = useDocument("lists", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <p className="loading">Loading...</p>;
+  }
+
   return (
     <div>
-      <h1>Your list id: {id}</h1>
+      <h4>{document.title}</h4>
     </div>
   );
 };
